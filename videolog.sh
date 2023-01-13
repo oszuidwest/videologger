6c936cf52cc66e90b0f92eb22784057b2ec17545
Raymon Mens <r.mens@me.com>
2023-01-14 00:23:18 +0100

    Update videolog.sh

--- videolog.sh
+++ videolog.sh
@@ -5,7 +5,7 @@ TIMESTAMP=$(/usr/bin/date +"%Y-%m-%d_%Hu") #YYYY-MM-DD_UUu
 # TODO: Define time to keep files
 
 # Check for ffmpeg
-if ! command -v ffmpeg &> /dev/null
+if ! command -v ffmpeg
 then
     echo "FFmpeg not found. Installing..."
     sudo apt-get install -y ffmpeg
@@ -23,6 +23,7 @@ else
 fi
 
 # Log for 1h
+echo "Starting log for 1h..."
 ffmpeg -y -hide_banner -loglevel error -i $STREAMURL -t 3600 -c:v copy -c:a copy $LOGDIR/$TIMESTAMP.mp4
 
 # TODO: Validation etc.
