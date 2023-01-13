adaed29e587c6b54c94cc935cd8a43a3698d5d9a
Raymon Mens <r.mens@me.com>
2023-01-14 00:20:14 +0100

    Check if dir exists

--- videolog.sh
+++ videolog.sh
@@ -13,6 +13,15 @@ else
     echo "FFmpeg is already installed."
 fi
 
+# Check for logdir
+if [ ! -d "$LOGDIR" ]; 
+then
+    echo "Logdir not found. Creating it.."
+    mkdir -p $LOGDIR
+else
+    echo "Logdir found. Nothing to do. Good!"
+fi
+
 # Log for 1h
 ffmpeg -hide_banner -loglevel error -i $STREAMURL -t 3600 -c:v copy -c:a copy $LOGDIR/$TIMESTAMP.mp4
 
